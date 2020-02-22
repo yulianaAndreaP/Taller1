8f5389bd7cbfee843f8da38fe3bb12655865d05e
yulianaAndreaP <yulapani@gmail.com>
2020-02-21 21:16:37 -0500

    Primer ejercicio

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo ("hola mundo");
+echo ("Generalidades PHP1 y desarrolle los siguientes ejercicios de Programacion Yuliana Paniagua <br>");
+
+echo "<br> Ejercicio 1";
+
+$numero1=500;
+$numero2=1000;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+
+
+
 
 
 
